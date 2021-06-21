3114573acfbe8e830a668bb11b39cb407411687d
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-21 22:03:26 +0500

    Solution for no_consecutive_repeats

--- lib/problems.rb
+++ lib/problems.rb
@@ -14,5 +14,22 @@ end
 # Write a method no_consecutive_repeats?(arr) that accepts an array as an arg. The method should return true if an element never appears consecutively in the array; it should return false otherwise.
 
 def no_consecutive_repeats?(arr)
-  
+  array=[]
+  return true if arr.length == 1
+  arr.each_with_index do |ele1, i1|
+    arr.each_with_index do |ele2, i2|
+      if i2 == i1 + 1
+        if arr[i1] == arr[i2]
+          return false
+        end
+      end
+    end
+  end
+  true
 end
+
+puts no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+puts no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+puts no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+puts no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+puts no_consecutive_repeats?(['x'])                              # => true
