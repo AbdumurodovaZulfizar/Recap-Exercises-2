32eae64018315ca501cd04326b47987434fc709c
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-21 18:14:47 +0500

    Solution for no_dupes

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,4 +1,12 @@
 # Write a method no_dupes?(arr) that accepts an array as an arg and returns an new array containing the elements that were not repeated in the array.
 def no_dupes?(arr)
-
+  array = []
+  hash = Hash.new(0)
+  arr.each do |ele|
+    hash[ele] += 1
+  end
+  hash.each do |k, v|
+    array << k if v == 1
+  end
+  array
 end
