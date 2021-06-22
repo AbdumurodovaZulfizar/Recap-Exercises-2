c53e30f9b306b76d950fc910e950445822b9dcca
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-22 14:52:17 +0500

    Create tests for bi_prime

--- rspec/problems_rspec.rb
+++ rspec/problems_rspec.rb
@@ -30,10 +30,30 @@ end
 
 describe "longest_streak" do
   it "should return the longest part of the same letter of string" do
-    expect(longest_streak('a'))
-    expect(longest_streak('accccbbb'))
-    expect(longest_streak('aaaxyyyyyzz'))
-    expect(longest_streak('aaabbb'))
-    expect(longest_streak('abc'))
+    expect(longest_streak('accccbbb')).to eq('cccc')
+    expect(longest_streak('aaaxyyyyyzz')).to eq('yyyyy')
+  end
+
+  it "should return the letter when there is only one" do
+    expect(longest_streak('a')).to eq('a')
+  end
+
+  it "should return the first part letter when all parts are equal" do
+    expect(longest_streak('aaabbb')).to eq('aaa')
+    expect(longest_streak('abc')).to eq('a')
+  end
+end
+
+describe "bi_prime?" do
+  it "should return true when the number is multiply of prime numbers" do
+    expect(bi_prime?(14)).to eq(true)
+    expect(bi_prime?(22)).to eq(true)
+    expect(bi_prime?(25)).to eq(true)
+    expect(bi_prime?(94)).to eq(true)
+  end
+
+  it "should return false if the number is not multiply of only prime numbers" do
+    expect(bi_prime?(24)).to eq(false)
+    expect(bi_prime?(64)).to eq(false)
   end
 end
