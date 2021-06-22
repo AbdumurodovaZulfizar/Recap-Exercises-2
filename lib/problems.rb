606ee2d974b430b83ceaccee5e857e7100073d1d
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-22 09:45:21 +0500

    Add instructions for my_map

--- lib/problems.rb
+++ lib/problems.rb
@@ -113,3 +113,9 @@ def my_select
     self.each_char { |ele| selected_str += ele if yield(ele)}
     selected_str
 end
+
+# Extend the string class by defining a String#map! method that accepts a block. The method should modify the existing string by replacing every character with the result of calling the block, passing in the original character and it's index. Do not use the built-in Array#map or Array#map! in your solution.
+
+def my_map
+  
+end
